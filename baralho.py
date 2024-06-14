d9630e6778fe12c7c6e942c8a6b3cd1253b48ea1
Samuel Damasceno <156391711+samueldamasceno@users.noreply.github.com>
2024-06-13 21:45:20 -0300

    classes para cartas e baralhos criadas

---
+++ baralho.py
@@ -0,0 +1,28 @@
+class Carta:
+    def __init__(self, nome, valor, naipe):
+        self.nome = nome
+        self.valor = valor
+        self.naipe = naipe
+
+class Baralho:
+    def __init__(self):
+        naipes = ['Espadas', 'Copas', 'Paus', 'Ouros']
+        valores = {
+            'A': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            '10': 10,
+            'J': 10,
+            'Q': 10,
+            'K': 10
+        }
+        self.cartas = []
+        for naipe in naipes:
+            for valor in valores:
+                self.cartas.append(Carta(valor, valores[valor], naipe))
