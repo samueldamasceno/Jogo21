aa370fb412d19f7050d283947eb3843f1d2d3e1a
Samuel Damasceno <156391711+samueldamasceno@users.noreply.github.com>
2024-06-13 21:49:19 -0300

    arquivo principal atualizado para importar baralho

--- baralho.py
+++ baralho.py
@@ -1,33 +1,52 @@
+# jogo de 21 no terminal em python
+
 import random
+import os
 
-class Carta:
-    def __init__(self, nome, valor, naipe):
-        self.nome = nome
-        self.valor = valor
-        self.naipe = naipe
+import baralho
 
-class Baralho:
-    def __init__(self):
-        naipes = ['Espadas', 'Copas', 'Paus', 'Ouros']
-        valores = {
-            'A': 1,
-            '2': 2,
-            '3': 3,
-            '4': 4,
-            '5': 5,
-            '6': 6,
-            '7': 7,
-            '8': 8,
-            '9': 9,
-            '10': 10,
-            'J': 10,
-            'Q': 10,
-            'K': 10
-        }
-        self.cartas = []
-        for naipe in naipes:
-            for valor in valores:
-                self.cartas.append(Carta(valor, valores[valor], naipe))
+def bem_vindo():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Bem-vindo ao jogo de 21!")
+    print()
+    while True:
+        print("""Você conhece as regras?
+              1. Sim
+              2. Não""")
+        resposta = input("Digite a opção que deseja: ")
+        if resposta == "1":
+            print()
+            print("Ótimo! Então vamos começar!")
+            print()
+            break
+        elif resposta == "2":
+            regras()
+        else:
+            print()
+            print("Opção inválida!")
+            print()
 
-    def embaralhar(self):
-        random.shuffle(self.cartas)
+def regras():
+    print()
+    print("Regras do jogo:")
+    print()
+    print("""
+        1. O objetivo é obter uma mão com um valor total mais próximo de 21 do que o oponente, sem ultrapassar.
+        2. Ás vale 1 ponto. As cartas de 2 a 10 valem seu valor normal. J, Q e K valem 10 pontos cada.
+        3. Em cada rodada, você tem a opção de comprar uma carta ou parar de comprar.
+        4. Se você comprar uma carta, ela será adicionada à sua mão.
+        5. Se você parar de comprar, sua mão será comparada com a mão do oponente.
+        6. Se a sua mão for maior que 21, você perde.
+        7. Se a sua mão for menor que a do oponente, você ganha.
+        8. Se as duas mãos forem iguais, você perde.""")
+    print()
+    print("Boa sorte!")
+    print()
+
+def iniciar_jogo():
+    bem_vindo()
+    
+
+
+
+iniciar_jogo()
